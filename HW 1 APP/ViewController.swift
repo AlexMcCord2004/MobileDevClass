a0d6b8ef08aa04c297c4d644aded3e1b51d638a3
Alex McCord <armccord04@gmail.com>
2026-01-11 17:44:40 -0600

    Initial Commit

---
+++ HW 1 APP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW 1 APP
+//
+//  Created by Alex McCord on 1/11/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
